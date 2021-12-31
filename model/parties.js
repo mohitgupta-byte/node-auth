197d9837509e59de627f54477a1791f28333fef5
mohitgupta-byte <mohitguptakarnal1999@gmail.com>
2021-12-31 17:29:39 +0530

    first commit

---
+++ model/parties.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const partiesSchema = new mongoose.Schema({ 
+
+    name:{
+        type:String,
+        trim:true,
+        require:true,
+        maxlenth:32
+    },
+    address:{
+        type:String,
+        require:true,
+    },
+    phone:{
+        type:Number,
+        trim:true,
+        require:true,
+        maxlenth:13
+    },
+    gst:{
+        type:String,
+        required:true,
+    },
+    outstand:{
+        type:String,
+        required:true,
+    },
+    pin:{
+        type:Number,
+        required:true,     
+    },
+    
+    
+},{timestamps:true})
+
+
+module.exports = mongoose.model("Parties",partiesSchema);
---
+++ model/product.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const{ObjectId} = mongoose.Schema;
+const productSchema = new mongoose.Schema({ 
+
+    name:{
+        type:String,
+        trim:true,
+        require:true,
+        maxlenth:32
+    },
+    description:{
+        type:String,
+        require:true,
+    },
+    price:{
+        type:Number,
+        trim:true,
+        require:true,
+        maxlenth:32
+    },
+    category:{
+        type:ObjectId,
+        ref:'Category',
+        required:true,
+    },
+   
+    quantity:{
+        type:Number,
+        
+    },
+    sold:{
+        type:Number,
+        default:0
+        
+    },
+    photo:{
+        data:Buffer, 
+        contentType:String
+    },
+    shipping:{
+        type:Boolean,
+        required:false
+
+    }
+    
+},{timestamps:true})
+
+
+module.exports = mongoose.model("Product",productSchema);
